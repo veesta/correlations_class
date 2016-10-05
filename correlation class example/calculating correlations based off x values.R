bf80bd2882bbef3d5cba9d64b2758721ed83e7cf
veesta <vmavanda@uoguelph.ca>
2016-10-04 22:53:02 -0400

    update markdown with more detail

---
+++ correlation class example/calculating correlations based off x values.R
@@ -0,0 +1,15 @@
+x <- c(4,6.83, 4,5.17, 5,5.33, 5.67,3.17, 3.83,3.67)
+y <- c(6.33,5.5, 5,5.17, 5.5,3.5, 5.3,5.17, 4.83,5.17)
+cor.test(x,y)
+
+data: x and y 
+t = 82191000, df = 3, p-value < 2.2e-16
+alternative hypothesis: true correlation is not equal to 0 95 percent confidence interval: 
+  1 1
+sample estimates:
+  cor
+1
+
+  n=100
+random sample of 10 people
+calculate the population's correlation
--- correlation_markdown.R
+++ correlation_markdown.R
@@ -1,22 +1,32 @@
 library(tidyverse)
+
+#load data from online source 
 my.data <- read_csv("https://jeroenooms.github.io/multilevel/data/chapter2/popular2.csv")
 
-#extract relevant columns and create analytic data 
+#may want to save data - see handout 
+#write_csv(my.data,path="hox_data_chapter2.csv")
+
+
+
+#extract relevant 4 columns and create analytic data 
 
 analytic.data <- select(my.data, class, extrav, popular, texp)
 
 # don't create factors leads to not being able to say greater than or less than so we keep them as a numeric value 
+#if it is categorical, need to make it numerical 
 
-#extrav column from the data set
+#correlation between 2 variables - extraversion and popularity 
 analytic.data$extrav
 analytic.data$popular
 
-#confidence interval of the correlation 
+#using what we defined above, calculate the correlation between the 2 with confidence intervals - below
 cor.test(x=analytic.data$extrav,y=analytic.data$popular)
 
 #i would report r=.32, 95% CI[.28,.35], p<.001
 
-#store correlation information as a variable 
+#UNDERSTANDING HOW R WORKS --- 
+
+#We can store correlation information as a variable to use in subsequent analyses 
 
 my.cor.result <- cor.test(x=analytic.data$extrav,y=analytic.data$popular)
 
@@ -26,61 +36,68 @@ my.cor.result
 #or just get the basic correlation estimate 
 my.cor.result$estimate
 
-#plotting a single correlation 
+#plotting a single correlation
 
 my.scatter <- qplot(x=extrav,y=popular,data=analytic.data)
 
 print(my.scatter)
+#note: not an apa graph, but make it that way with other handouts 
 
-#add regression line 
+#add regression line with geom_smooth, make it linear with 'lm', get a regression line with se=TRUE, don't want one, se=FALSE
+#help = ?geom_smooth
 
-my.scatter <- qplot(x=extrav,y=popular,data=analytic.data)
 my.scatter <- my.scatter + geom_smooth(method = "lm", se = TRUE, color="black")
 print(my.scatter)
 
-#make se = TRUE to get a grey confidence interval and = FALSE for no confidence interval 
-
-#calculate several correlations
+#CALCULATE SEVERAL CORRELATIONS ----
 
 #convert data set to a data frame 
 
 library(apaTables)
 apa.cor.table(as.data.frame(analytic.data))
 
+#View multiple scatter plots for all our data with the pairs.panels command from the psych package
+#lm=TRUE makes sure we get a regression line 
+
 psych::pairs.panels(as.data.frame(analytic.data),lm=TRUE)
 
-#SUBGROUP ANALYSIS 
+#SUBGROUP ANALYSIS - correlation for a single subgroup, i.e. females only ------------
+#need tidyverse
+#can swap out class with another variable
 
 analytic.data.class.is.5 <- filter(analytic.data,class==5)
 
 #View(analytic.data.class.is.5) to see that we have just class 5's info 
 
-#get the correlation for class 5
+#get the correlation for class 5 - extrav & popularity 
 
 cor.test(x=analytic.data.class.is.5$extrav,y=analytic.data.class.is.5$popular)
 
-#calculate correlations for several subgroups - need tidyverse package 
+#calculate correlations for several subgroups - need tidyverse package ---------------
 
+#group it by class
 analytic.data.grouped.by.class <- group_by(analytic.data, class)
 
 #correlation for each group 
 
 cor.by.group <- summarise(analytic.data.grouped.by.class, cor.test(x=extrav,y=popular)$estimate)
 
+#print to make sure it got all 100 
 print(cor.by.group)
 
-#print all the rows
-
+#print all the rows - not with a short form of (with 90 more rows)
 print(as.data.frame(cor.by.group))
 
-#plotting correlations for several groups 
+#PLOTTING CORRELATIONS FOR SEVERAL GROUPS ---------------
 
+#pick the subtest of groups you want, 15 to 30 using filter - need tidyverse
 analytic.data.groups.15.to.30 <- filter(analytic.data.grouped.by.class, class>=15 & class<=30)
 
 #create scatter plot for this data 
 
 my.scatter <- qplot(x=extrav,y=popular,data=analytic.data.groups.15.to.30)
 my.scatter <- my.scatter + facet_wrap(~class)
+print(my.scatter)
 
 #add regression line 
 
